c971bcd5fdcf0e02ee6621526537000f58abe230
Cynthia Akanaga <akana22c@Mtholyoke.edu>
2023-02-25 17:57:11 -0500

    Update

---
+++ src/components/Home.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+
+const Home = () => {
+    return (
+        <div>
+            <main>
+                <section id="hero">
+                    <h2>Get sustainable food and build a better world, <strong>one bite at a time.</strong></h2>
+                    <p>Our major goal is to give all of the items in one location so that farmers can easily purchase seeds and fertilizers while also providing the necessary seed and product knowledge.</p>
+
+                </section>
+            </main>
+
+            
+        </div>
+
+    );
+};
+
+export default Home;
