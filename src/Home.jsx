b678cddc5290cec9f8b0d5601c5333293305e885
saha23s <saha23s@mtholyoke.edu>
2023-02-26 07:42:22 -0500

    working version

---
+++ src/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Home = () => {
+  return(
+    <div className='main'>
+        <section>
+            <h2>Get sustainable food and build a better world, one bite at a time.</h2>
+          </section>
+    </div>
+  );
+}
+
+export default Home;
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-
+import './Card.css';
 
 const Card = (props) => {
   return(
     <div className='bg-light-green dib br3 pa3 ma2 bw2 shadow-5'>
       {/* <img alt = 'items' src = {`${props.items.lisiting_image}`} style = {{width:200 , height:200}} /> */}
-      <div>
+      <div className='card'>
         {console.log(props)}
         <h2>{props.items.listing_name}</h2>
-        <p>{props.items.listing_desc}</p>
-        {/* <Link to='/details' state= {props}> Know In Details </Link> */}
+        <a href={`${props.items.media_website}`}> Website </a>
+        <p> Contact: {props.items.contact_name} ({props.items.contact_email})</p>
+        <p> Location: {props.items.location_address}</p>
       </div>
     </div>
   );
