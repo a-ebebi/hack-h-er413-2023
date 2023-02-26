2c9ef359cc43a4e69e7119083de36c4012e23b04
Cynthia Akanaga <akana22c@Mtholyoke.edu>
2023-02-26 00:05:56 -0500

    Update landing page

--- src/App.jsx
+++ src/App.jsx
@@ -14,15 +14,7 @@ import About from './components/About';
 
 function App() {
   return (
-    <>
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/about" component={About} />
-          <Redirect to="/" />
-        </Switch>
-      </Router>
-    </>
+    
 
   );
 }
